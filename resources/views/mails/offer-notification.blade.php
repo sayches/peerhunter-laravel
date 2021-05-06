c4d788c3424bdb24bb9cb90258f49f8e8d746232
MPqJXDwBc88Y <61336552+mdPT8UyRWfj27c3@users.noreply.github.com>
2021-05-06 03:13:39 +0100

    Update offer-notification.blade.php

--- resources/views/mails/offer-notification.blade.php
+++ resources/views/mails/offer-notification.blade.php
@@ -9,11 +9,15 @@
                     <p style="letter-spacing: 0.8px; font-size:14px;line-height:20px;color:#858585;margin-bottom: 30px; font-family: Roboto,Helvetica,Arial,sans-serif; color: #5a5656;">
                         {{$bodyMessage}} #{{$offer->id}}
                     </p>
-                    <p style="letter-spacing: 0.8px;font-size:12px;font-style:italic; color: #5a5656;">If you have any
-                        questions or concerns regarding this, please feel free to get in touch with us at <b>support@sayches.com</b>
-                    </p>
-                    <h3 style="margin: 40px 0px 8px 0; font-size: 16px;">Thanks &amp; Regards,</h3>
-                    <p style="font-size: 14px; margin: 0; color: #5a5656;">PeerHunter Team</p>
+                    <div class="content email_temp" style="margin: 0px auto; font-family: Roboto,Helvetica,Arial,sans-serif; text-align: center !important;">
+<div class="container">
+<div class="email_body" style="padding: 20px;">
+<div class="row">
+<div class="col-md-12">
+<p style="letter-spacing: 0.8px; font-size: 12px; font-style: italic; color: #5a5656;"><strong>You Are at Your Own Risk:</strong> You understand and agree that your use of PeerHunter is at your sole risk. We are NOT an escrow service. We do not take any responsibility for transactions made through or outside PeerHunter.</p>
+<p style="letter-spacing: 0.8px; font-size: 12px; font-style: italic; color: #5a5656;"><strong>Stay Safe:</strong> To avoid becoming a victim of scammers, NEVER transfer cryptocurrency before receiving the payment! Don&rsquo;t believe anyone who claims to be a customer support and convinces you to complete the transaction before you receive the payment - they&rsquo;re scammers. Always verify the buyer&rsquo;s reputation, understand payment reversals, meet in public places and ask a lot of questions.</p>
+<p style="letter-spacing: 0.8px; font-size: 12px; font-style: italic; color: #5a5656;">If you have any questions or concerns regarding this, please feel free to get in touch with us at <strong>support@sayches.com</strong></p>
+                    <h3 style="margin: 40px 0px 8px 0; font-size: 16px;">Best!</h3>
                 </div>
             </div>
 
